a09cace8f505885421673216b3ee63a4ddcb98ed
aravinthHash <aravinthHash@gmail.com>
2021-03-18 12:20:07 +0530

    test changes

--- IceCream/IceCreamApp/src/test/java/com/iceCream/app/IceCreamApplication.java
+++ IceCream/IceCreamApp/src/test/java/com/iceCream/app/IceCreamApplication.java
@@ -3,9 +3,9 @@ package com.iceCream.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication    
+/*@SpringBootApplication    
 public class IceCreamApplication {  
 	public static void main(String[] args) {    
 		SpringApplication.run(IceCreamApplication.class, args);    
 	}  
-}
+}*/
