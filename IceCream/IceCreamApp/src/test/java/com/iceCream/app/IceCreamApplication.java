50360c750479428fa3e53fdf89a6a5d706a603a4
Kaleeswari <aravinthHash@gmail.com>
2021-03-18 11:29:30 +0530

    setup

---
+++ IceCream/IceCreamApp/src/test/java/com/iceCream/app/IceCreamApplication.java
@@ -0,0 +1,11 @@
+package com.iceCream.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication    
+public class IceCreamApplication {  
+	public static void main(String[] args) {    
+		SpringApplication.run(IceCreamApplication.class, args);    
+	}  
+}
